ef1126353a8366327702ee72610d88e5f004f70e
yuma4869 <tttyyy76@outlook.jp>
2024-04-29 22:33:33 +0900

    :coffin: alpinejsのimportmap削除、erbにscriptで記述することでOK

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # pin "custom/main"
-
-pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.13.9/dist/module.esm.js"
-pin "alpine-turbo-drive-adapter", to: "https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.1.0/dist/alpine-turbo-drive-adapter.esm.js"
